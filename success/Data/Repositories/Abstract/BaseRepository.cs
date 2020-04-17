13c4e93d5f831733cf7425e8d5e58c011d92b292
Jandere <iutzhanov@bk.ru>
2020-04-18 00:36:12 +0600

    [add] AddRepositoryPattern

---
+++ success/Data/Repositories/Abstract/BaseRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using success.Data.Context;
+using success.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace success.Data.Repositories.Abstract
+{
+    public class BaseRepository<TEntity> : ICRUDRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        public async Task CreateAsync(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAsync()
+        {
+            return await _dbSet.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TEntity> GetAsync(int id)
+        {
+            return await _dbSet.FirstOrDefaultAsync(e => e.Id == id) ?? null;
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _dbSet.Update(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ success/Data/Repositories/Abstract/ICRUDRepository.cs
@@ -0,0 +1,17 @@
+﻿using success.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace success.Data.Repositories.Abstract
+{
+    interface ICRUDRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task CreateAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAsync();
+        Task<TEntity> GetAsync(int id);
+        Task DeleteAsync(TEntity entity);
+    }
+}
